800bfe56ef3e3f3b9ba123f3fad2283643ad6ca9
ZAKRI <mostafazakri3@gmail.com>
2025-08-05 15:26:18 +0100

    add circle

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,23 @@ async fn main() {
     let mut frame_count: u32 = 0; 
 
     loop {
+        clear_background(BLACK);
         if is_mouse_button_released(MouseButton::Left) {
             let p1 = mouse_position();
             println!("{:?}", p1);
             px.push(p1);
         }
+        if !px.is_empty()  {
+            for i in 0..px.len() {
+                draw_circle(px[i].0, px[px.len()-1].1, 3.0, WHITE);
+            }  
+        }
 
-        clear_background(BLACK);
-
-        // if !px.is_empty() {
-        //     for i in 0..px.len() - 1 {
-        //         draw_line(px[i].0, px[i].1, px[i + 1].0, px[i + 1].1, 1.0, RED);
-        //     }
-        // }
+        if !px.is_empty() && px.len() > 2  && is_key_released(KeyCode::Enter) {
+            for i in 0..px.len() - 1 {
+                draw_line(px[i].0, px[i].1, px[i + 1].0, px[i + 1].1, 1.0, RED);
+            }
+        }
 
         if is_key_released(KeyCode::Enter) {
             
@@ -62,7 +66,7 @@ async fn main() {
             }
 
             frame_count += 1;
-            if frame_count % 100 == 0 {
+            if frame_count % 120 == 0 {
                 step += 1;
                 if step > max_steps {
                     step = max_steps;
