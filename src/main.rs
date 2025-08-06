29a06d78b96c2300ab04f3fd5e409800a25bde84
obenali <oussama.benali@ump.ac.ma>
2025-08-06 21:59:18 +0100

    add warnings

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,30 @@ use macroquad::prelude::*;
 
 #[macroquad::main("chaikin")]
 async fn main() {
-    // initial points
     let mut px: Vec<(f32, f32)> = Vec::new();
-    // current step
     let mut step = 0;
     let mut animation_started = false;
-
+    
     let max_steps = 6; 
-    // vector of vectors to store the next set of points 
     let mut smoothy: Vec<Vec<(f32, f32)>> = Vec::new();
-    // counting frames
     let mut frame_count: u32 = 0; 
-    // speed of animation 
     let animation_speed = 60; 
+
+    let warning_text = "Warning: \n\tThere should be at least 2 points to start animation";
+    let mut warning = false; 
 
     loop {
         clear_background(BLACK);
-
-        let mut warning = String::from("hello world");
-        draw_text(&warning, 50.0, 100.0, 50.0, RED);
         
-        let mut step_text = if animation_started {
+        let step_text = if animation_started {
             format!("Step: {}/{}", step + 1, max_steps + 1)
         } else {
             format!("Step: 0/{}", max_steps + 1)
         };
-        // text , x , y, color
-        draw_text(&step_text, 50.0, 50.0, 50.0, RED);
-        
+        draw_text(&step_text, 10.0, 30.0, 24.0, WHITE);
+        if warning {
+            draw_text(warning_text , 10.0, 70.0, 18.0, YELLOW);
+        } 
         if is_key_pressed(KeyCode::C) {
             animation_started = false;
             px.clear();
@@ -38,6 +34,7 @@ async fn main() {
         }
         
         if is_mouse_button_released(MouseButton::Left) && !animation_started {
+            warning = false;
             let p1 = mouse_position();
             px.push(p1);
         }
@@ -46,21 +43,19 @@ async fn main() {
                 draw_circle_lines(px[i].0, px[i].1, 3.0, 1.0, WHITE);
             }
         }
-        if  px.len() >= 2 && is_key_released(KeyCode::Enter) {
+        if px.len() >= 2 && is_key_released(KeyCode::Enter) {
             for i in 0..px.len() - 1 {
                 draw_line(px[i].0, px[i].1, px[i + 1].0, px[i + 1].1, 1.0, RED);
             }
-        }else if px.len() == 1 && is_key_released(KeyCode::Enter) {
-            println!("now it should change");
-            warning = "two points at least to start animating".to_string();
-            draw_text(&warning, 50.0, 100.0, 50.0, RED);
+        }else if px.len() < 2 && is_key_released(KeyCode::Enter)  {
+            warning = true;
         }
 
         if is_key_released(KeyCode::Enter) &&  px.len() >= 2  {
             smoothy.clear();
             smoothy.push(px.clone());
-            for i in 0..max_steps {
-                let prev = &smoothy[i];
+            for s in 0..max_steps {
+                let prev = &smoothy[s];
                 let mut new_px = Vec::new();
                 let n = prev.len();
                 new_px.push(prev[0]);
@@ -100,9 +95,10 @@ async fn main() {
         }
         
         if is_key_released(KeyCode::Escape) {
-            exit(1);
+            exit(0);
         }
 
         next_frame().await;
     }
 }
+
